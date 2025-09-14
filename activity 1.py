4b243b6d3cc3c6e2a6a938f1ec795ff010bea7c7
Noor13456 <noorgrps@gmail.com>
2025-09-14 18:54:16 +0500

    1

---
+++ activity 1.py
@@ -0,0 +1,24 @@
+'''
+
+
+
+
+
+'''                                                                 
+a='2'
+b='3'
+print(int(a)+int(b))
+print(type(a))
+print(type(b))
+a='2'
+b='3'
+c=int(a)
+d=int(b)
+print(c+d)
+print(type(c))
+print(type(d))
+
+#string concatenation
+firstname="Noor"
+lastname="Emaan"
+print(firstname+lastname)
